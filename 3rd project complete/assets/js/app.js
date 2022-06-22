6b83605fa9d44c80d83fd5734f63682bb80b7b65
bhoopendrasingh123 <105096725+bhoopendrasingh123@users.noreply.github.com>
2022-06-22 21:14:58 +0530

    first commit

---
+++ 3rd project complete/assets/js/app.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    nav:false,
+    smartSpeed:700,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+            
+        }
+    }
+})
